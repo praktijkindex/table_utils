3d31c82b6d2a2e467383f65b0ba02963be2e752a
Artem Baguinski <abaguinski@depraktijkindex.nl>
2014-11-18 14:56:07 +0100

    log some more

--- lib/rake/table_import.rb
+++ lib/rake/table_import.rb
@@ -10,12 +10,13 @@ module Rake::DSL
         begin
           logger.info { "Importing #{source.table_name} from #{source.input_path}" }
           source.import progress_opts
-        rescue
+        rescue Exception => e
+          logger.fatal { "Error during import of '#{source.table_name}'" }
+          logger.fatal { e }
           if source.table_exists?
-            logger.warn { "dropping #{table_name} due to errors during import" }
+            logger.warn { "Dropping #{table_name} due to errors during import" }
             source.drop_table
           end
-          raise
         end
       end
     end
