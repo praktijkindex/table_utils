feb0b9d1e1532d3122747b5af3a051605d6d40ae
Artem Baguinski <abaguinski@depraktijkindex.nl>
2014-11-18 15:09:36 +0100

    shorter log messages

--- lib/rake/table_import.rb
+++ lib/rake/table_import.rb
@@ -1,4 +1,5 @@
 require "data_source"
+require "active_support/core_ext/string"
 
 module Rake::DSL
   def table_import args, &block
@@ -12,7 +13,7 @@ module Rake::DSL
           source.import progress_opts
         rescue Exception => e
           logger.fatal { "Error during import of '#{source.table_name}'" }
-          logger.fatal { e }
+          logger.fatal { shorten_strings(e) }
           if source.table_exists?
             logger.warn { "Dropping #{table_name} due to errors during import" }
             source.drop_table
@@ -20,5 +21,11 @@ module Rake::DSL
         end
       end
     end
+  end
+
+  def shorten_strings obj, width=80
+    message = obj.to_s.split("\n").map {|line|
+      line.truncate(width)
+    }.join("\n")
   end
 end
